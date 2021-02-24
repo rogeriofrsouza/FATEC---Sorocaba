32fd86cde76828916555e546534d6d98f2d3d59c
Rogério Ferreira de souza <rogeriofrsouza@gmail.com>
2021-02-24 15:28:13 -0300

    Add files via upload

---
+++ Ex001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int num;
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &num);
+    printf("O numero digitado foi %d", num);
+    system("pause");
+}
---
+++ hello world.c
@@ -0,0 +1,16 @@
+/*comentário
+Este é o meu primeiro programa
+Arquivo criado em: 17/02/2021
+Horário: 10h34
+Autor: Rogério
+*/
+#include <stdio.h>
+/*#include <stdlib.h>
+system("notepad")*/
+
+void main ()
+{
+    printf("Hello World \n");
+    system("calc");
+    system("pause");
+}
